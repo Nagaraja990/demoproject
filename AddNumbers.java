c296bc5472f678372521738e30792ce8b5fe305b
Nagaraja990 <118419040+Nagaraja990@users.noreply.github.com>
2022-12-22 09:29:11 +0530

    Add files via upload

---
+++ AddNumbers.java
@@ -0,0 +1,10 @@
+ class AddNumbers
+{
+   public static void main(String[] args)
+   {
+      int numberOne = 5, numberTwo = 10, add;
+      
+      add = numberOne + numberTwo;
+      System.out.println("Result = " +add);
+   }
+}
